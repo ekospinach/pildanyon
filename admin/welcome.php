79257659d41ea53b73da3cf4cceb2c146dc954bc
Eko Andi Anggriawan <ekoandi007@gmail.com>
2021-03-10 16:58:59 +0800

    Add files via upload

---
+++ admin/welcome.php
@@ -0,0 +1,4 @@
+<div class="welcome">
+   <h2>Selamat Datang di PILDANYON</h2>
+   <p style="font-size:18px;">Anda login sebagai <strong><?php echo $_SESSION['user']; ?></strong> dengan hak akses admin</p>
+</div>
